e48cc621ebd69360fb74ec53cdf8daf011080032
Jaume Planas <jaume.planas@minorisa.net>
2018-10-05 15:13:42 +0200

    mrp.workorder

--- addons/mrp/models/__init__.py
+++ addons/mrp/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import ir_attachment
+import mrp_workorder
 import mrp_config_settings
 import mrp_bom
 import mrp_message
@@ -9,7 +10,6 @@ import mrp_routing
 import mrp_workcenter
 import mrp_production
 import mrp_unbuild
-import mrp_workorder
 import procurement
 import product
 import res_company
