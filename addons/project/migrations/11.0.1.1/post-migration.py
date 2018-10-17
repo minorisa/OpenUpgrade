dcb3efefe6e70a160119028acf681c04a974fc3f
Jaume Planas <jaume.planas@minorisa.net>
2018-10-17 12:39:48 +0200

    11.0 Post-migration get Employee ID from User ID in account.analytic.line

--- addons/project/migrations/11.0.1.1/post-migration.py
+++ addons/project/migrations/11.0.1.1/post-migration.py
@@ -222,3 +222,14 @@ def migrate(env, version):
         env.cr, 'project', ['mail_template_data_module_install_project'],
     )
     remove_mail_subtypes(env)
+
+    # Minorisa
+    # Convert User IDs in Employee IDs in account.analytic.line
+    openupgrade.logged_query(
+        env.cr, """
+        UPDATE account_analytic_line SET employee_id = hr_employee.id
+        FROM hr_employee, resource_resource
+        WHERE hr_employee.resource_id = resource_resource.id
+        AND account_analytic_line.user_id = resource_resource.user_id
+        """
+    )
